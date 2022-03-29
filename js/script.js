f327588fd4e34e748ad0ffc964e59572df7c5a5a
Vladislav Torgovtsev <vlad4990@gmail.com>
2022-03-30 02:30:20 +0300

    ver 1

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+            isMobile.Android()
+            || isMobile.BlackBerry()
+            || isMobile.iOS()
+            || isMobile.Opera()
+            || isMobile.Windows()
+        );
+    }
+};
+if (isMobile.any()) {
+    document.body.classList.add('_touch')
+} else {
+    document.body.classList.add('_pc')
+}
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body')
+if (iconMenu) {
+    iconMenu.addEventListener('click', () => {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active')
+        menuBody.classList.toggle('_active')
+    })
+}
+
+
+const pageHeader = document.querySelector('.header');
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset) {
+        pageHeader.classList.add(
+            "_scrolled"
+        )
+    } else {
+        pageHeader.classList.remove(
+            "_scrolled"
+        )
+    }
+})
